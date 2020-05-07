22af829eec9889cf993c1434de7c125faefdb93c
WellseeC <38332041+WellseeC@users.noreply.github.com>
2020-05-07 06:00:11 -0400

    ui.py update

--- code/ui.py
+++ code/ui.py
@@ -1,20 +1,28 @@
 import tkinter as tk
 import time
 import os
+from queries import *
 
+#function for Lookup NBA players button
 def search_nba_players():
 	var = entry.get()
-	print(var)
+	nba_player_lookup(var)
 
+#function for Lookup NCAA players button
 def search_ncaa_players():
 	var = entry.get()
-	print(var)
+	ncaa_player_lookup(var)
 
+#Main
 if __name__ == '__main__':
+
+	#Window setup
 	window = tk.Tk()
 	window.title('Database System Project - Basketball')
 	window.geometry('600x450')
 	window.resizable(width=False, height=False)
+
+	#Menu gif
 	image_file = [tk.PhotoImage(file='dear-basketball.gif', format='gif -index %i' %(i)) for i in range(9)]
 	def update(ind):
 		if ind == 9:
@@ -26,11 +34,15 @@ if __name__ == '__main__':
 	label = tk.Label(window)
 	label.pack()
 	window.after(0, update, 0)
+
+	#Menu entry
 	entry = tk.Entry(window, show=None, width=30)
 	entry.pack()
-	nba = tk.Button(window, text='Check on NBA players', width=30, height=2, command=search_nba_players)
+
+	#Menu button
+	nba = tk.Button(window, text='Lookup NBA players', width=30, height=2, command=search_nba_players)
 	nba.pack()
-	ncaa = tk.Button(window, text='Check on NCAA players', width=30, height=2, command=search_ncaa_players)
+	ncaa = tk.Button(window, text='Lookup NCAA players', width=30, height=2, command=search_ncaa_players)
 	ncaa.pack()
 	window.mainloop()
 
